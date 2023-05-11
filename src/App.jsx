a551f877a623cb16ec2929626cd5bd99bbad4ba8
Aakansha <aakanshasingh769@gmail.com>
2023-05-11 08:57:27 +0530

    solve the error

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Menu from './Menu'
-import { Routes, Route } from "react-router-dom"
-import Home from './Home'
-import categories from "./categories.json"
-import Cart from './Cart'
-import CartContext from "./contexts/CartContext"
-import Footer from './Footer'
-import "./styles/app.scss"
-import SharedStateContext from './contexts/SharedStatesContext'
-import FinalPage from './FinalPage'
+import React from 'react';
+import Navbar from './Navbar';
+import Menu from './Menu';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import categories from './categories.json';
+import Cart from './Cart';
+import CartContext from './contexts/CartContext';
+import Footer from './Footer';
+import './styles/app.scss';
+import SharedStateContext from './contexts/SharedStatesContext';
+import FinalPage from './FinalPage';
 
 export default function App() {
-
-  const [isCartVisible, setIsCartVisible] = React.useState(false)
+  const [isCartVisible, setIsCartVisible] = React.useState(false);
 
   return (
     <>
@@ -21,40 +20,43 @@ export default function App() {
         <Navbar setIsCartVisible={setIsCartVisible} />
 
         <CartContext>
-          <Cart isCartVisible={isCartVisible}
+          <Cart
+            isCartVisible={isCartVisible}
             setIsCartVisible={setIsCartVisible}
           />
 
           <Routes>
-            <Route exact path='menu' element={
-              <Menu categoryId={null} />
-            } />
+            <Route exact path="menu" element={<Menu categoryId={null} />} />
 
-            {categories.map((iterator) =>
-              <Route path={`menu/${iterator.id}`} element={
-                <Menu categoryId={iterator.id} />}
-              />)}
+            {categories.map(category => (
+              <Route
+                key={category.id}
+                path={`/menu/${category.id}`}
+                element={<Menu categoryId={category.id} />}
+              />
+            ))}
 
-            <Route index element={
-              <Home />
-            } />
+            <Route index element={<Home />} />
 
-            <Route path='*' element={
-              <h1>error 404:<br />Page Not Found</h1>}
+            <Route
+              path="*"
+              element={
+                <h1>
+                  error 404:
+                  <br />
+                  Page Not Found
+                </h1>
+              }
             />
 
-            <Route exact path='thanks' element={
-              <FinalPage />}
-            />
+            <Route exact path="thanks" element={<FinalPage />} />
           </Routes>
-
         </CartContext>
       </SharedStateContext>
 
-
-      <footer id="footer"><Footer /></footer>
+      <footer id="footer">
+        <Footer />
+      </footer>
     </>
-  )
+  );
 }
-
-
